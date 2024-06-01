9f5eba975fb81151ab30abcffd15b60cecf2a910
Prabeg Shakya <shakyaprabeg@gmail.com>
2024-06-01 15:10:39 +0545

    Add route for login
    Signed-off-by: Prabeg Shakya <shakyaprabeg@gmail.com>

--- backend/src/routes/auth.route.ts
+++ backend/src/routes/auth.route.ts
@@ -5,12 +5,13 @@ import { tryCatchWrapper } from "../utils/try-catch.util";
 import { UserService } from "../modules/users/user.service";
 import { validate } from "../middlewares/validator.middleware";
 import { AuthController } from "../modules/auth/auth.controller";
-import { registrationSchema } from "../modules/auth/auth.schema";
 import { KnexUserRepository } from "../modules/users/knex-user.repository";
+import { registrationSchema, loginSchema } from "../modules/auth/auth.schema";
 
 const router: Router = Router();
 const authController = new AuthController(new UserService(new KnexUserRepository(dbConn)));
 
 router.post("/register", validate(registrationSchema), tryCatchWrapper(authController.register));
+router.post("/login", validate(loginSchema), tryCatchWrapper(authController.login));
 
 export default router;
