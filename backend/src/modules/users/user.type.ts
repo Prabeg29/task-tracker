b041fda88aa48363b4ee21569930acefd1ea0ce8
Prabeg Shakya <shakyaprabeg@gmail.com>
2024-06-01 11:03:54 +0545

    Define type for user, input and response
    Signed-off-by: Prabeg Shakya <shakyaprabeg@gmail.com>

---
+++ backend/src/modules/users/user.type.ts
@@ -0,0 +1,26 @@
+type User = {
+  id: number;
+  name: string;
+  email: string;
+  password?: string;
+  role: number;
+  createdAt: Date;
+  updatedAt: Date;
+  deletedAt?: Date;
+};
+
+type CreateUserDto = Omit<User, 'id' | 'createdAt' | 'updatedAt' | 'deletedAt'>;
+
+type UserResponseDto = {
+  id: number;
+  attributes: {
+    name: string,
+    email: string;
+    role: string;
+    createdAt: string;
+    updatedAt: string;
+  },
+  token?: string,
+}
+
+export { User, CreateUserDto, UserResponseDto };
