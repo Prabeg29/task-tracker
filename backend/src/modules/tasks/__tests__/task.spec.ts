680d59f333c51f0d98bf5ba8b4d5a31976d98298
Prabeg Shakya <shakyaprabeg@gmail.com>
2024-06-02 16:48:09 +0545

    Add blueprint for tests
    Signed-off-by: Prabeg Shakya <shakyaprabeg@gmail.com>

---
+++ backend/src/modules/tasks/__tests__/task.spec.ts
@@ -0,0 +1,21 @@
+describe("Tasks API", () => {
+  describe("GET: /api/tasks", () => {
+
+  });
+
+  describe("POST: /api/tasks", () => {
+
+  });
+
+  describe("GET: /api/tasks/:id", () => {
+
+  });
+
+  describe("PUT: /api/tasks/:id", () => {
+
+  });
+
+  describe("DELETE: /api/tasks/:id", () => {
+
+  });
+});
