b5ba897c42a449c3816edb37cf40b536789747fa
Prabeg Shakya <shakyaprabeg@gmail.com>
2024-06-03 12:05:11 +0545

    Add unit test for fetching a single task
    Signed-off-by: Prabeg Shakya <shakyaprabeg@gmail.com>

--- backend/src/modules/tasks/__tests__/tasks-service.spec.ts
+++ backend/src/modules/tasks/__tests__/tasks-service.spec.ts
@@ -13,9 +13,11 @@ describe("Task Service", () => {
   let error: HttpException;
 
   let mockCreate: jest.SpyInstance<Promise<TaskWithUsers>, [taskData: TaskCreateDto]>;
+  let mockFetchOneById: jest.SpyInstance<Promise<TaskWithUsers | undefined>, [id: number]>;
 
   beforeEach(() => {
     mockCreate = jest.spyOn(knexTaskRepository, "create");
+    mockFetchOneById = jest.spyOn(knexTaskRepository, "fetchOneById");
   });
 
   afterEach(() => jest.clearAllMocks());
@@ -27,6 +29,31 @@ describe("Task Service", () => {
       res = await taskService.create(createTaskDtoStub());
 
       expect(mockCreate).toHaveBeenCalledWith(createTaskDtoStub());
+      expect(res).toMatchObject(taskStub()[0]);
+    });
+  });
+
+  describe("fetchOneById", () => {
+    it("should throw error when user doesn\"t exist", async () => {
+      mockFetchOneById.mockResolvedValue(undefined);
+
+      try {
+        res = await taskService.fetchOneById(23);
+      } catch (err) {
+        error = err as HttpException;
+      }
+
+      expect(mockFetchOneById).toHaveBeenCalledWith(23);
+      expect(error).toBeInstanceOf(HttpException);
+      expect(error.message).toBe("Task with the given id does not exists");
+      expect(error.statusCode).toBe(404);
+    });
+
+    it("should return the existing task", async () => {
+      mockFetchOneById.mockResolvedValue(taskStub()[0]);
+
+      res = await taskService.fetchOneById(taskStub()[0].id);
+
       expect(res).toMatchObject(taskStub()[0]);
     });
   });
