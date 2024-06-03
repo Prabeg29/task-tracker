15eef5a8f2aa4c9e50bb2c4ab02cb569eb850118
Prabeg Shakya <shakyaprabeg@gmail.com>
2024-06-03 09:53:18 +0545

    Add permission for roles
    Signed-off-by: Prabeg Shakya <shakyaprabeg@gmail.com>

--- backend/src/config/index.ts
+++ backend/src/config/index.ts
@@ -1,24 +1,38 @@
-import path from 'path';
+import path from "path";
 
-import * as dotenv from 'dotenv';
+import * as dotenv from "dotenv";
 
-dotenv.config({ path: path.join(__dirname, '../../.env')});
+dotenv.config({ path: path.join(__dirname, "../../.env")});
 
 export default {
   app: {
     port    : process.env.APP_PORT || 3000,
-    url     : process.env.NODE_ENV === 'test' ? 'http://localhost:3000' : process.env.APP_URL,
-    logLevel: process.env.LOG_LEVEL || 'info',
+    url     : process.env.NODE_ENV === "test" ? "http://localhost:3000" : process.env.APP_URL,
+    logLevel: process.env.LOG_LEVEL || "info",
   },
   db: {
-    client  : process.env.DB_CONNECTION || 'mysql2',
-    host    : process.env.NODE_ENV === 'test' ? process.env.DB_TEST_HOST : process.env.DB_HOST,
+    client  : process.env.DB_CONNECTION || "mysql2",
+    host    : process.env.NODE_ENV === "test" ? process.env.DB_TEST_HOST : process.env.DB_HOST,
     port    : process.env.DB_PORT || 3306,
-    database: process.env.NODE_ENV === 'test' ? process.env.DB_TEST_DATABASE : process.env.DB_DATABASE,
-    user    : process.env.DB_USERNAME || 'task-tracker',
-    password: process.env.DB_PASSWORD || ''
+    database: process.env.NODE_ENV === "test" ? process.env.DB_TEST_DATABASE : process.env.DB_DATABASE,
+    user    : process.env.DB_USERNAME || "task-tracker",
+    password: process.env.DB_PASSWORD || ""
   },
   secrets: {
     jwt: process.env.JWT_SECRET,
-  }
+  },
+  permissions: [
+    {
+      "role": "super_admin",
+      "permissions": ["create_tasks", "read_tasks", "update_tasks", "delete_tasks"]
+    },
+    {
+      "role": "admin",
+      "permissions": ["create_tasks", "read_tasks", "update_tasks"]
+    },
+    {
+      "role": "member",
+      "permissions": ["create_tasks", "read_tasks", "update_tasks"]
+    }
+  ],
 };
