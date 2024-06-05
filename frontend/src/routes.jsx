77eb9d26e25edd30fa2d22d7930586fad345a018
Prabeg Shakya <shakyaprabeg@gmail.com>
2024-06-05 16:32:10 +0545

    Remove unused routes from sidenav
    Signed-off-by: Prabeg Shakya <shakyaprabeg@gmail.com>

--- frontend/src/routes.jsx
+++ frontend/src/routes.jsx
@@ -1,12 +1,10 @@
 import {
-  HomeIcon,
-  UserCircleIcon,
   TableCellsIcon,
-  InformationCircleIcon,
   ServerStackIcon,
   RectangleStackIcon,
 } from "@heroicons/react/24/solid";
-import { Home, Profile, Tasks, Notifications } from "@/pages/dashboard";
+
+import { Tasks } from "@/pages/dashboard";
 import { Login, Register } from "@/pages/auth";
 
 const icon = {
@@ -18,28 +16,10 @@ export const routes = [
     layout: "dashboard",
     pages: [
       {
-        icon: <HomeIcon {...icon} />,
-        name: "dashboard",
-        path: "/home",
-        element: <Home />,
-      },
-      {
-        icon: <UserCircleIcon {...icon} />,
-        name: "profile",
-        path: "/profile",
-        element: <Profile />,
-      },
-      {
         icon: <TableCellsIcon {...icon} />,
         name: "tasks",
         path: "/tasks",
         element: <Tasks />,
-      },
-      {
-        icon: <InformationCircleIcon {...icon} />,
-        name: "notifications",
-        path: "/notifications",
-        element: <Notifications />,
       },
     ],
   },
--- frontend/src/widgets/layout/sidenav.jsx
+++ frontend/src/widgets/layout/sidenav.jsx
@@ -2,7 +2,6 @@ import PropTypes from "prop-types";
 import { Link, NavLink } from "react-router-dom";
 import { XMarkIcon } from "@heroicons/react/24/outline";
 import {
-  Avatar,
   Button,
   IconButton,
   Typography,
@@ -49,18 +48,7 @@ export function Sidenav({ brandImg, brandName, routes }) {
       <div className="m-4">
         {routes.map(({ layout, title, pages }, key) => (
           <ul key={key} className="mb-4 flex flex-col gap-1">
-            {title && (
-              <li className="mx-3.5 mt-4 mb-2">
-                <Typography
-                  variant="small"
-                  color={sidenavType === "dark" ? "white" : "blue-gray"}
-                  className="font-black uppercase opacity-75"
-                >
-                  {title}
-                </Typography>
-              </li>
-            )}
-            {pages.map(({ icon, name, path }) => (
+            {layout !== "auth" && pages.map(({ icon, name, path }) => (
               <li key={name}>
                 <NavLink to={`/${layout}${path}`}>
                   {({ isActive }) => (
