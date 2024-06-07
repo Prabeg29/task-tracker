e01a3e09628003f1cb55cb9ba0bcaf11d95e69dc
Prabeg Shakya <shakyaprabeg@gmail.com>
2024-06-07 15:44:15 +0545

    Test: put api, should update task from authenticated user allowed, having a valid task input
    Signed-off-by: Prabeg Shakya <shakyaprabeg@gmail.com>

--- backend/src/modules/tasks/__tests__/task.spec.ts
+++ backend/src/modules/tasks/__tests__/task.spec.ts
@@ -237,7 +237,43 @@ describe("Tasks API", () => {
       expect(response.status).toEqual(StatusCodes.UNPROCESSABLE_ENTITY);
     });
 
-    it.skip("should update task from authenticated user allowed, having a valid task input", () => { });
+    it("should update task from authenticated user allowed, having a valid task input", async () => {
+      const user = await (new KnexUserRepository()).create({
+        name    : "Hari Bahadur",
+        email   : "hari.bahadur@mahajodi.com",
+        password: bcrypt.hashSync("P@ssword123$", 5),
+        role    : 1
+      });
+
+      const task = await (new KnexTaskRepository()).create(user.id, {
+        title: "Write a script"
+      });
+
+      response = await request(server)
+        .post("/api/auth/login")
+        .set("Accept", "application/json")
+        .send({ "email": "hari.bahadur@mahajodi.com", "password": "P@ssword123$" });
+
+      token = response.body.user.accessToken;
+
+      response = await request(server)
+        .put(`/api/tasks/${task.id}`)
+        .set("Accept", "application/json")
+        .set("Authorization", `Bearer ${token}`)
+        .send({
+          "title"      : "Write a script update",
+          "description": faker.string.alphanumeric(100),
+          "assignedTo" : 1,
+          "status"     : "wip",
+        })
+
+      expect(response.status).toEqual(StatusCodes.OK);
+      expect(response.body.message).toEqual("Task updated successfully");
+      expect(response.body.task.attributes.title).toEqual("Write a script update");
+      expect(response.body.task.attributes.status).toEqual("wip");
+      expect(response.body.task.relationships.createdBy.attributes.email).toEqual("hari.bahadur@mahajodi.com");
+      expect(response.body.task.relationships.assignedTo.attributes.email).toEqual("hari.bahadur@mahajodi.com");
+    });
   });
 
   describe("DELETE: /api/tasks/:id", () => {
