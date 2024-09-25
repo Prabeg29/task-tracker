58c13b93aa3cd8abf5aad2cf2f52cab62b8f269e
Prabeg Shakya <shakyaprabeg@gmail.com>
2024-09-25 17:52:22 +0545

    [x] - Introduce auth service for google oauth
    Signed-off-by: Prabeg Shakya <shakyaprabeg@gmail.com>

--- backend/src/modules/auth/auth.controller.ts
+++ backend/src/modules/auth/auth.controller.ts
@@ -1,29 +1,30 @@
 import { Request, Response } from "express";
 
+import { User } from "../users/user.type";
+import { AuthService } from "./auth.service";
 import { UserMapper } from "../users/user.mapper";
-import { UserService } from "../users/user.service";
-import { CreateUserDto, User } from "../users/user.type";
 import { StatusCodes } from "../../enums/status-codes.enum";
 
 export class AuthController {
   constructor(
-    protected readonly userService: UserService,
+    protected readonly authService: AuthService,
   ) { }
 
-  public register = async (req: Request, res: Response): Promise<void> => {
-    const user: User & { accessToken: string; refreshToken: string; } = await this.userService.create(req.body as CreateUserDto);
-
-    res.status(StatusCodes.CREATED).json({ message: "Registration successful.", user: UserMapper.toResponseDto(user) });
+  public googleConsent =  (_req: Request, res: Response): void => {
+    res.status(StatusCodes.OK).json({ 
+      message: "Please visit the following link",
+      link   : this.authService.getConsentUrl()
+    });
   };
 
-  public login = async (req: Request, res: Response): Promise<void> => {
-    const user: User & { accessToken: string; } = await this.userService.signin(req.body as Partial<CreateUserDto>);
+  public googleCallback = async (req: Request, res: Response): Promise<void> => {
+    const user: User & { accessToken: string; refreshToken: string; } = await this.authService.login(req.query.code as string);
 
-    res.status(StatusCodes.OK).json({ message: "Login successful.", user: UserMapper.toResponseDto(user) });
+    res.status(StatusCodes.OK).json({ user: UserMapper.toResponseDto(user) });
   };
 
   public generateAccessToken = async (req: Request, res: Response): Promise<void> => {
-    const accessToken = await this.userService.generateAccessToken(req.body.refreshToken);
+    const accessToken = await this.authService.generateAccessToken(req.body.refreshToken);
 
     res.status(StatusCodes.OK).json({ accessToken: accessToken });
   };
---
+++ backend/src/modules/auth/auth.service.ts
@@ -0,0 +1,72 @@
+import jwt from "jsonwebtoken";
+
+import config from "../../config";
+import { User } from "../users/user.type";
+import { roles } from "../../enums/roles.enum";
+import { GoogleService } from "../../lib/google.service";
+import { StatusCodes } from "../../enums/status-codes.enum";
+import { HttpException } from "../../exceptions/http.exception";
+import { UserRepositoryInterface } from "../users/user.irepository";
+
+export class AuthService {
+  private readonly googleService: GoogleService;
+
+  constructor(
+    private readonly userRepository: UserRepositoryInterface
+  ) {
+    this.googleService = new GoogleService(config.google.oauthClientId, config.google.oauthClientSecret);
+  }
+
+  public getConsentUrl(): string {
+    return this.googleService.getAuthorizationUrl(config.google.redirectUrl, config.google.scopes.split(","));
+  }
+
+  public async login(code: string | null): Promise<User & { accessToken: string; refreshToken: string; }> {
+    if (!code) {
+      throw new HttpException("Authorization code not found", StatusCodes.NOT_FOUND);
+    }
+
+    const token = await this.googleService.getToken(code, config.google.redirectUrl);
+    const accessToken = token.access_token;
+
+    if (!accessToken) {
+      throw new HttpException("Failed to get access token", StatusCodes.UNPROCESSABLE_ENTITY);
+    }
+
+    const userInfo = await this.googleService.getUserInfo(accessToken);
+
+    const isExistingUser = await this.userRepository.fetchOneByEmail(userInfo.email);
+
+    if (!isExistingUser) {
+      throw new HttpException("Invalid credentials", StatusCodes.UNAUTHORIZED);
+    }
+
+    return {
+      ...isExistingUser,
+      accessToken: jwt.sign(
+        { id: isExistingUser.id, email: isExistingUser.email, role: roles[isExistingUser.role] }, 
+        config.secrets.jwt, 
+        { expiresIn: "10m" }
+      ),
+      refreshToken: jwt.sign(
+        { id: isExistingUser.id, email: isExistingUser.email, role: roles[isExistingUser.role] }, 
+        config.secrets.refreshJwt, 
+        { expiresIn: "7d" }
+      )
+    };
+  }
+
+  public async generateAccessToken(refreshToken: string): Promise<string> {
+    if (!refreshToken) {
+      throw new HttpException("Invalid credentials", StatusCodes.UNAUTHORIZED);
+    }
+
+    const user = jwt.verify(refreshToken, config.secrets.refreshJwt) as { id: number; email: string; role: string };
+
+    return jwt.sign(
+      { id: user.id, email: user.email, role: user.role }, 
+      config.secrets.jwt, 
+      { expiresIn: "10m" }
+    );
+  }
+}
